8452f1058739d6bb24adb94e5e664160f36d5a39
Spencer Gregson <sgregson@wayfair.com>
2015-11-30 12:38:46 -0500

    Site updated: 2015-11-30 12:38:46

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var tables = document.querySelectorAll('.post-content > table')
+for (var i = 0; i < tables.length; i++) {
+  tables[i].setAttribute('class','pure-table pure-table-bordered')
+}
+
+var imgs = document.querySelectorAll('.post-content img')
+for (var i = 0; i < imgs.length; i++) {
+  imgs[i].setAttribute('class','pure-img')
+}
+
+// Shorthand for $( document ).ready()
+$(function() {
+  console.log('hi');
+
+  var $sidebarTrigger = $('.js-sidebar-trigger'),
+      $layout = $('#layout');
+
+  $sidebarTrigger.on('click', function() {
+    $layout.toggleClass('is-withSidebar');
+  });
+});
